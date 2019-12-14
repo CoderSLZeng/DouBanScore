f4c4ef30d704b5c529da7a1481efb37889c0a1f8
SLCoderZeng <359297567@qq.com>
2019-12-14 09:22:03 +0800

    登录’

---
+++ Products/wechat-miniprogram/pages/login/login.js
@@ -0,0 +1,17 @@
+// pages/login/login.js
+Page({
+
+  wechatLogin: function() {
+    console.log('wechatLogin');    
+  },
+
+  doubanLogin: function() {
+    console.log('doubanLogin');    
+  },
+  
+  openAgreement: function() {
+    wx.navigateTo({
+      url: '/pages/agreement/agreement',
+    });   
+  }
+})
